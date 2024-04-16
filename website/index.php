9c4275f53c86f7b28243168657178500df1f4b57
r-witz <r.witz@proton.me>
2024-04-16 22:40:54 +0200

    feat: homepage main frame

---
+++ website/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CINEPLEX</title>
+    <link rel="stylesheet" href="styles/homepage.css">
+</head>
+<body>
+    <div id="mainview">
+        <h1>CINEPLEX</h1>
+        <img src="img/3d/camera.webp" alt="Camera" id="camera">
+        <img src="img/3d/glasses.webp" alt="Glasses" id="glasses">
+        <img src="img/3d/spectator_chair.webp" alt="Spectator Chair" id="chair">
+        <img src="img/3d/clap.webp" alt="Clap" id="clap">
+        <img src="img/3d/popcorn_cup.webp" alt="Popcorn Cup" id="popcorn_cup">
+        <img src="img/3d/popcorn.webp" alt="Popcorn" id="pop_1">
+        <img src="img/3d/popcorn.webp" alt="Popcorn" id="pop_2">
+        <img src="img/3d/popcorn.webp" alt="Popcorn" id="pop_3">
+        <img src="img/3d/popcorn.webp" alt="Popcorn" id="pop_4">
+        <img src="img/icons/arrow.webp" alt="Arrow" id="down_arrow">
+    </div>
+    <div id="presentation"></div>
+    <div id="fade"></div>
+    <div id="cinema_room"></div>
+</body>
+</html>
