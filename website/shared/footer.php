ecff2864312ca54bde151ce1c574f6111d613aff
Alexis <aperrier301@gmail.com>
2024-04-17 12:43:55 +0200

    feat : footer front

---
+++ website/shared/footer.php
@@ -0,0 +1,39 @@
+
+    <footer>
+        <link rel="stylesheet" href="../styles/footer.css">
+        <div class="left">
+            <div class="text_left">
+                <h3>Follow us</h3>
+            </div>
+            <div class="img_left">
+                <a href="#"><img src="../img/icons/instagram.webp" alt=""></a>
+                <a href="#"><img src="../img/icons/x.webp" id="x" alt=""></a>
+                <a href="#"><img src="../img/icons/facebook.webp" id="facebook" alt=""></a>
+                <a href="#"><img src="../img/icons/tiktok.webp" id="tiktok" alt=""></a>
+            </div>
+        </div>
+
+        <div class="middle">
+            <div class="title_middle">
+                <h2>CINEPLEX</h2>
+            </div>
+            <div class="infos_middle">
+                <p>Privacy Policy | Term and Policies | Ad Choices | Accessibility</p>
+            </div>
+            <div class="copyright_middle">
+                <p>@ 2024 CINEPLEX</p>
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="text_right">
+                <h3>Contact us</h3>
+            </div>
+            <div class="img_right">
+                <a href="#"><img src="../img/icons/mail.webp" alt=""></a>
+                <a href="#"><img src="../img/icons/form.webp" alt=""></a>
+            </div>
+        </div>
+    </footer>
+  </body>
+</html>
