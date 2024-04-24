119cbbdc9bdeb3d3d43889976850fb65cb544733
r-witz <r.witz@proton.me>
2024-04-24 14:38:04 +0200

    feat: search + categories implemented on navbar

--- website/shared/header.php
+++ website/shared/header.php
@@ -3,17 +3,17 @@
 <header>
     <div class="navbar">
         <div class="left">        
-            <a href="index.php"><img src="../img/icons/accueil.webp" class="imgheader" alt=""></a>
+            <a href="/index.php"><img src="../img/icons/accueil.webp" class="imgheader" alt=""></a>
             <div class="dropdown-container">
                 <p class="headerp">CATEGORIES</p>
                 <img src="../img/icons/plain_arrow.webp" id="plainarrow" class="imgheader" alt="">
                 <div class="dropdown-menu grid">
-                <a href="#"><span>Drama</span></a>
-                <a href="#"><span>Action</span></a>
-                <a href="#"><span>Comedy</span></a>
-                <a href="#"><span>Horror</span></a>
-                <a href="#"><span>Romance</span></a>
-                <a href="#"><span>Thriller</span></a>
+                <a href="/search.php?search=Drama"><span>Drama</span></a>
+                <a href="/search.php?search=Action"><span>Action</span></a>
+                <a href="/search.php?search=Crime"><span>Crime</span></a>
+                <a href="/search.php?search=Sci-Fi"><span>Sci-Fi</span></a>
+                <a href="/search.php?search=Romance"><span>Romance</span></a>
+                <a href="/search.php?search=Thriller"><span>Thriller</span></a>
                 </div>
                 </div>
             </div>
@@ -21,8 +21,10 @@
         <div class="right">
             <input class="checkbox" type="checkbox">
             <div class="search">
-                <input class="search_input" type="text">
-                <img id="searchIcon" src="../img/icons/loupe.webp" class="imgheader" alt="">
+                <form action="/actions/search.php" method="post">
+                    <input class="search_input" name="search" type="text">
+                    <img id="searchIcon" src="../img/icons/loupe.webp" class="imgheader" alt="">
+                </form>
             </div>
             <a href="#"><img src="../img/icons/library.webp" class="imgheader" alt=""></a>
 
