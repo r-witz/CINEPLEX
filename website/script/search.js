54d7af11e461c61fbde6fc393e9e15fd99177c2a
r-witz <r.witz@proton.me>
2024-04-24 13:11:59 +0200

    feat(search): carousel scroll automatically when we click on image, film information change on film image click

--- website/script/search.js
+++ website/script/search.js
@@ -9,5 +9,34 @@ for (let i = 0; i < images.length; i++) {
         oldSelectedImage.removeAttribute("id");
 
         this.id = "selected";
+        updateFilmInfos();
+        moveCarousel();
     });
 }
+
+const getSelectedImagePosition = () => {
+    const selectedImage = document.getElementById("selected");
+    const selectedImagePosition = Array.from(images).indexOf(selectedImage);
+
+    return selectedImagePosition;
+};
+
+const updateFilmInfos = () => {
+    const selectedImagePosition = getSelectedImagePosition();
+    const filmContainer = document.querySelector("#container");
+
+    filmContainer.style.left = `-${selectedImagePosition * 100}vw`;
+};
+
+const moveCarousel = () => {
+    const selectedImagePosition = getSelectedImagePosition();
+    const filmContainer = document.getElementById("film-carousel");
+
+    const containerWidth = filmContainer.offsetWidth;
+    const imageWidth = images[0].offsetWidth;
+    const scrollPosition = ((selectedImagePosition + 1) * (imageWidth + 48)) - (containerWidth / 2);
+
+    console.log(selectedImagePosition, containerWidth, imageWidth, scrollPosition);
+
+    filmContainer.scrollLeft = scrollPosition;
+};
